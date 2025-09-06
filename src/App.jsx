e74ee9d5ea435ffd24efed6bc28518355067eacc
Sarakawachi <kawachi@kdl.co.jp>
2025-09-06 12:23:57 +0900

    データ取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import { supabase } from "./utils/supabase";
 
 function App() {
   const [titleText, setTitleText] = useState("");
@@ -8,6 +9,19 @@ function App() {
   const [errorMessage, setErrorMessage] = useState("");
   const [sumTime, setSumTime] = useState();
   const isError = !titleText || !timeText;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getRecord = async () => {
+      setLoading(true);
+      const { data } = await supabase.from("study-record").select();
+      console.log(data);
+      setRecords(data);
+      setLoading(false);
+    };
+
+    getRecord();
+  }, []);
 
   const changeTitleText = (e) => setTitleText(e.target.value);
   const changeTimeText = (e) => setTimeText(e.target.value);
@@ -41,36 +55,42 @@ function App() {
 
   return (
     <>
-      <h1>学習記録一覧</h1>
-      <div className="form">
-        <div className="study-content">
-          <p>学習内容</p>
-          <input type="text" value={titleText} onChange={changeTitleText} />
-        </div>
-        <div className="study-content">
-          <p>学習時間</p>
-          <input type="number" value={timeText} onChange={changeTimeText} />
-          <p>時間</p>
-        </div>
-        <button className="add-button" onClick={addRecord}>
-          登録
-        </button>
-      </div>
-      <div className="error-message"> {errorMessage}</div>
-      <div>
-        <p>入力されている学習内容：{titleText}</p>
-        <p>学習内入力されている時間：{timeText}時間</p>
-      </div>
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <>
+          <h1>学習記録一覧</h1>
+          <div className="form">
+            <div className="study-content">
+              <p>学習内容</p>
+              <input type="text" value={titleText} onChange={changeTitleText} />
+            </div>
+            <div className="study-content">
+              <p>学習時間</p>
+              <input type="number" value={timeText} onChange={changeTimeText} />
+              <p>時間</p>
+            </div>
+            <button className="add-button" onClick={addRecord}>
+              登録
+            </button>
+          </div>
+          <div className="error-message"> {errorMessage}</div>
+          <div>
+            <p>入力されている学習内容：{titleText}</p>
+            <p>学習内入力されている時間：{timeText}時間</p>
+          </div>
 
-      <div className="content-list">
-        <ul>
-          {records.map((record, index) => (
-            <li key={index}>{`${record.title} ${record.time}時間`}</li>
-          ))}
-        </ul>
-      </div>
+          <div className="content-list">
+            <ul>
+              {records.map((record, index) => (
+                <li key={index}>{`${record.title} ${record.time}時間`}</li>
+              ))}
+            </ul>
+          </div>
 
-      <p>合計時間：{sumTime}/1000</p>
+          <p>合計時間：{sumTime}/1000</p>
+        </>
+      )}
     </>
   );
 }
