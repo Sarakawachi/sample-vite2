844cee322ed0c6b9309ddca7d93f05f1fa3ee721
Sarakawachi <kawachi@kdl.co.jp>
2025-10-28 09:25:08 +0900

    jest dotenv設定

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config();
 
-// https://vite.dev/config/
 export default defineConfig({
+  define: {
+    "process.env.VITE_SUPABASE_URL": JSON.stringify(process.env.VITE_SUPABASE_URL),
+    "process.env.VITE_SUPABASE_KEY": JSON.stringify(process.env.VITE_SUPABASE_KEY),
+  },
   plugins: [react()],
-})
+});
